4c5e755b30730ed768f293aed00ada139c68d644
valymirauta <mirautav@gmail.com>
2020-09-08 22:26:18 +0300

    add update option

--- src/main/java/com/valy/springboot/service/EmployeeService.java
+++ src/main/java/com/valy/springboot/service/EmployeeService.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface EmployeeService {
     List<Employee> getAllEmployees();
     void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
 }
